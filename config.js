f8fd91b5f9f4e020642c93de08391a498191af24
jyotir07 <jyotir.aditya.tf@gmail.com>
2025-02-18 23:47:56 +0530

    cleaned the code

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvp5LN_NVHpo5agYrm6FaOukgWTZzU8w8",
+  authDomain: "buddybars-c9915.firebaseapp.com",
+  databaseURL: "https://buddybars-c9915-default-rtdb.firebaseio.com",
+  projectId: "buddybars-c9915",
+  storageBucket: "buddybars-c9915.firebasestorage.app",
+  messagingSenderId: "223283177016",
+  appId: "1:223283177016:web:1d09bb5d6df854f1bc28cd",
+  measurementId: "G-5E71RXW1LS",
+};
+
+// Exporting the config object for use in other files
+export default firebaseConfig;
